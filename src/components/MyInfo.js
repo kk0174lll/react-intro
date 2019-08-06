e385875baf376ba62cc47a773b7e5ef8fd609b9d
kk0174lll <kk0174lll@gmail.com>
2019-08-06 19:56:01 +0500

    Lesson 8

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+      <div>
+        <h1> er wer </h1>
+        <p> rew wer wer </p>
+        <ul>
+          <li>wrewer</li>
+          <li>wrewer</li>
+          <li>wer erwe ewr</li>
+        </ul>
+      </div>
+    )
+  }
+  
+export default MyInfo
