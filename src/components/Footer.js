1aeed2c736146ff0ebc67c464cbacaffd4e20053
kk0174lll <kk0174lll@gmail.com>
2019-08-12 15:53:22 +0500

    Lesson 36

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,30 @@
-import React from "react"
+import React, { Component } from "react"
 
-function Footer() {
-    return (
-        <footer>This is the footer</footer>
-    )
+class Footer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoading: true
+        }
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 1500)
+    }
+    render() {
+        return (
+            <div>
+                {this.state.isLoading ?
+                <h1>Loading...</h1> :
+                <footer>This is the footer</footer>}
+            </div>
+            
+        )
+    }
+
 }
 
 export default Footer
